ebdf859cb343fd02efafe3983cc6f093b7698e0f
Akira Nomura <anomura824@gmail.com>
2025-03-10 09:52:11 +0900

    [Fix] update model architecture from MobileNetV3-small to MobileNetV3-large

--- CNN-Transfer-Learning/Flower_image_classification/model.py
+++ CNN-Transfer-Learning/Flower_image_classification/model.py
@@ -1,14 +1,9 @@
 import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader, Subset, random_split
-import torchvision
-from torchvision import transforms
-from torchvision.models import mobilenet_v3_small
-from torchvision.models.mobilenetv3 import MobileNet_V3_Small_Weights
+from torch import nn
+from torchvision.models import mobilenet_v3_large
+from torchvision.models.mobilenetv3 import MobileNet_V3_Large_Weights
 
-import matplotlib.pyplot as plt
-
-def load_pretrained_model(base_model=mobilenet_v3_small, weights=MobileNet_V3_Small_Weights, num_classes=102, params_path=None):
+def load_pretrained_model(base_model=mobilenet_v3_large, weights=MobileNet_V3_Large_Weights, num_classes=102, params_path=None):
     """
     Load a pre-trained model and change the classifier to the number of classes in the dataset.
     """
@@ -20,7 +15,7 @@ def load_pretrained_model(base_model=mobilenet_v3_small, weights=MobileNet_V3_Sm
         model = base_model(weights=weights)
 
     # change 1000 classes classifier to 102 classes classifier
-    model.classifier[-1] = nn.Linear(in_features=1024, out_features=num_classes, bias=True)
+    model.classifier[-1] = nn.Linear(in_features=1280, out_features=num_classes, bias=True)
 
     # load trained parameters
     model.load_state_dict(torch.load(params_path, map_location=torch.device('cpu')))
